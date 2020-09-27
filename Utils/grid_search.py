8387dc449c7c0aa7da756a17449efef32341cd1d
Julien <julien.vignoud@epfl.ch>
2020-09-27 19:47:31 +0200

    Comment grid_search.py

--- Utils/grid_search.py
+++ Utils/grid_search.py
@@ -19,11 +19,11 @@ def get_best_parameters(w0, w1, losses):
 
 def grid_search(y, tx, w0, w1):
     """
-    :param y:
-    :param tx:
-    :param w0:
-    :param w1:
-    :return:
+    :param y: labels
+    :param tx: features
+    :param w0: list of values for the first parameter
+    :param w1: list of values for the second parameter
+    :return: array of losses where losses[i,j] is the loss computed using w0[i] and w1[j]
     """
     losses = np.zeros((len(w0), len(w1)))
     for i, w_0 in np.ndenumerate(w0):
