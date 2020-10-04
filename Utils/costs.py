db2bab4093fbcd2185c7d353d19749d89ee9aa16
Julien-Ben <33035980+Julien-Ben@users.noreply.github.com>
2020-10-04 09:44:35 +0200

    Adding one line of documentation

--- Utils/costs.py
+++ Utils/costs.py
@@ -5,6 +5,8 @@ import numpy as np
 
 def compute_loss(y, tx, w, loss_function='mse'):
     """
+    Computes and returns the loss using the given function in parameters.
+
     :param y: labels
     :param tx: features
     :param w: weights
