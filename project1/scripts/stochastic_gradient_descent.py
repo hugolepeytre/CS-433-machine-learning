a8a52f5ed76e18e6c0a690c1ee3d926b1ad522ab
hugolepeytre <hugo.lepeytre@gmail.com>
2020-10-18 12:24:41 +0200

    Started logistic regression

--- project1/scripts/stochastic_gradient_descent.py
+++ project1/scripts/stochastic_gradient_descent.py
@@ -18,18 +18,18 @@ def compute_stoch_gradient(y, tx, w, loss_function='mse'):
     return compute_gradient(y, tx, w, loss_function)
 
 
-def stochastic_gradient_descent(y, tx, initial_w, batch_size, max_iters, gamma, loss_function='mse'):
+def stochastic_gradient_descent(y, tx, initial_w, max_iters, gamma, loss_function='mse'):
     """
 
     :param y: Labels
     :param tx: Feature points
     :param initial_w: Initial weights
-    :param batch_size: Size of random subsets of features at each step
     :param max_iters: Number of iterations of gradient descent
     :param gamma: Learning Rate
     :param loss_function: loss_function: Which loss function to use (mse or mae for now)
     :return: History of losses and weights through descent
     """
+    batch_size = 100
     ws = [initial_w]
     losses = []
     w = initial_w
