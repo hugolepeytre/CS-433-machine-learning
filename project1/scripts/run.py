8947086765e9386759e0976294be4a7d9b2153d2
hugolepeytre <hugo.lepeytre@gmail.com>
2020-10-25 20:13:10 +0100

    Finished commenting

--- project1/scripts/run.py
+++ project1/scripts/run.py
@@ -8,56 +8,88 @@ DATA_TRAIN_PATH = DATA_FOLDER + 'train.csv'
 DATA_TEST_PATH = DATA_FOLDER + 'test.csv'
 
 # Parameters of ridge regression
-DEGREES = [5, 5, 5, 9]
-LAMBDA_ = 0.0003
+RIDGE_DEGREES = [5, 5, 5, 9]
+RIDGE_LAMBDA_ = 0.0003
 
 # Parameters of logistic regression
-GAMMAS = [0.14, 0.08, 0.14, 0.08]
-DEGREE = 3
-MAX_ITERS = 600
+LOGISTIC_GAMMAS = [0.14, 0.08, 0.14, 0.08]
+LOGISTIC_DEGREE = 3
+LOGISTIC_MAX_ITERS = 600
 
 
 def fit_ridge(y, x, ids):
+    """
+    Trains the ridge regression model using the hard-coded hyperparameters
+    :param y: Labels
+    :param x: Feature points
+    :param ids: Ids
+    :return: The trained weight vector
+    """
     ws = []
     losses = []
     for i in range(len(y)):
         y_cat, tx_cat, ids_cat = y[i], x[i], ids[i]
-        w, loss = model_data(y_cat, tx_cat, 'ridge_regression', poly_exp=DEGREES[i], lambda_=LAMBDA_)
+        w, loss = model_data(y_cat, tx_cat, 'ridge_regression', poly_exp=RIDGE_DEGREES[i], lambda_=RIDGE_LAMBDA_)
         ws.append(w)
         losses.append(loss)
     return ws
 
 
 def fit_logistic(y, x, ids):
+    """
+    Trains the logistic regression model using the hard-coded hyperparameters
+    :param y: Labels
+    :param x: Feature points
+    :param ids: Ids
+    :return: The trained weight vector
+    """
     ws = []
     losses = []
     for i in range(len(y)):
         y_cat, tx_cat, ids_cat = y[i], x[i], ids[i]
-        w, loss = model_data(y_cat, tx_cat, 'logistic_regression', poly_exp=DEGREE, gamma=GAMMAS[i], max_iters=MAX_ITERS)
+        w, loss = model_data(y_cat, tx_cat, 'logistic_regression',
+                             poly_exp=LOGISTIC_DEGREE, gamma=LOGISTIC_GAMMAS[i], max_iters=LOGISTIC_MAX_ITERS)
         ws.append(w)
         losses.append(loss)
     return ws
 
 
 def prediction_by_cat(model, w, x, ids):
-    # TODO : change
-    y_pred = np.array([])
-    ids_pred = np.array([])
+    """
+    Makes prediction using the trained weight vector, and associates them with ids
+    :param model: Type of model to train (logistic regression or ridge regression)
+    :param w: Trained weight vector
+    :param x: Unlabeled feature points
+    :param ids: Ids of feature points
+    :return: The predictions, with the associated ids
+    """
+    y_pred = np.empty(0)
+    ids_pred = np.empty(0)
     for i, (w_cat, tx_cat, ids_cat) in enumerate(zip(w, x, ids)):
         if model == 'ridge_regression':
-            tx_poly = build_poly_2D(tx_cat, DEGREES[i])
-            prediction = predict_labels(w, tx_poly)
+            tx_poly = build_poly_2D(tx_cat, RIDGE_DEGREES[i])
+            prediction = predict_labels(w_cat, tx_poly)
         elif model == 'logistic_regression':
-            tx_poly = build_poly_2D(tx_cat, DEGREE)
-            prediction = predict_labels_logistic(w, tx_poly)
+            tx_poly = build_poly_2D(tx_cat, LOGISTIC_DEGREE)
+            prediction = predict_labels_logistic(w_cat, tx_poly)
         else:
             raise ValueError("Wrong arguments : only 'ridge_regression' or 'logistic_regression' are available")
-        y_pred = np.concatenate((y_pred, prediction)) if len(y_pred) else prediction
-        ids_pred = np.concatenate((ids_pred, ids)) if len(ids_pred) else ids
+        y_pred = np.r_[y_pred, prediction]
+        ids_pred = np.r_[ids_pred, ids_cat]
     return y_pred, ids_pred
 
 
 def create_by_cat_submission(model, y_train, x_train, ids_train, x_test, ids_test):
+    """
+    Trains a model on the given data, does the predictions and creates a prediction file as ../data/submission.csv
+    :param model: Type of model to train (logistic regression or ridge regression)
+    :param y_train: Labels
+    :param x_train: Feature points
+    :param ids_train: Ids
+    :param x_test: Feature points of unlabeled test data
+    :param ids_test: Ids of unlabeled test data
+    :return: Nothing
+    """
     if model == 'ridge_regression':
         w = fit_ridge(y_train, x_train, ids_train)
     elif model == 'logistic_regression':
@@ -70,7 +102,12 @@ def create_by_cat_submission(model, y_train, x_train, ids_train, x_test, ids_tes
 
 
 def main():
-    if (len(sys.argv) == 2) & ('regression' in str(sys.argv[1])):
+    """
+    Main function. Loads the data, does data processing and makes prediction file.
+    Takes the argument given to the script. Only accepts 'ridge_regression' and 'logistic_regression'
+    :return: Nothing
+    """
+    if len(sys.argv) == 2:
         model = str(sys.argv[1])
         print("Loading training data...")
         y_train, tx_train, ids_train = load_csv_data(DATA_TRAIN_PATH)
